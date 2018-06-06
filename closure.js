fb092c9db149403100f57097bba23cfa77692430
heii70 <williamgao70@yahoo.com>
2018-06-06 06:38:52 +0000

    Done.

---
+++ closure.js
@@ -0,0 +1,68 @@
+/* Closures are nested functions that are able to access parent variables even after the parent function finishes. This is the way JS sets private variables and methods like the "private" keyword in Java and C++ */
+
+//------------------- Single nested method -------------------//
+/*
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+  
+  var myScope = outerFunction();
+  console.log(myScope());
+*/
+
+//------------------- Multiple nested methods -----------------//
+/*
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return {
+    
+    innerScope1: function() {
+      var inaccessible = "Nothing can touch me.";
+ 
+      return innerVariable;
+    },
+    
+    innerScope2: function() {
+      var inaccessible = "Nothing can touch me either.";
+ 
+      return innerVariable.replace('.', ' too.');
+    }
+    
+  }
+}
+
+var myScope = outerFunction();
+console.log(myScope.innerScope1());
+console.log(myScope.innerScope2());
+*/
+
+//------------------ Shortcut: Don't need "outerFunction" declaration------------------//
+
+var myScope = (function() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return {
+    
+    innerScope1: function() {
+      var inaccessible = "Nothing can touch me.";
+ 
+      return innerVariable;
+    },
+    
+    innerScope2: function() {
+      var inaccessible = "Nothing can touch me either.";
+ 
+      return innerVariable.replace('.', ' too.');
+    }
+    
+  }
+})()
+
+console.log(myScope.innerScope1());
+console.log(myScope.innerScope2());
