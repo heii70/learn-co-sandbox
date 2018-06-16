3d59e1cebfdbd5735f11aa01623c2b0dd8549149
heii70 <williamgao70@yahoo.com>
2018-06-16 08:17:33 +0000

    Done.

---
+++ index.rb
@@ -0,0 +1,15 @@
+class Dog
+
+  def set_name(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def get_name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.set_name("Lassie")
+ 
+puts lassie.get_name
